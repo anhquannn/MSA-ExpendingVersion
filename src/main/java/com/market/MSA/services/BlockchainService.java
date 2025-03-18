82e1ed4d9e76f011452d8cd520e8074c9f8d3ca3
anhquannn <nquankira@gmail.com>
2025-03-18 09:51:56 +0700

    Add EntityFinderService

---
+++ src/main/java/com/market/MSA/services/BlockchainService.java
@@ -0,0 +1,10 @@
+// package com.market.MSA.services;
+//
+// import org.springframework.stereotype.Service;
+//
+// @Service
+// public class BlockchainService {
+//	private Gateway gateway;
+//    private Network network;
+//    private Contract contract;
+// }
